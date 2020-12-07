b7b378ecf0129af4cf11b11b8c2cd882dba014e1
LeentechDev <adanhel.manicio@gmail.com>
2020-12-07 11:07:29 +0800

    VALORANT SCREEN ADDED

--- lib/bloc/game_request/game_request_bloc.dart
+++ lib/bloc/game_request/game_request_bloc.dart
@@ -16,25 +16,27 @@ class GameRequestBloc extends Bloc<GameRequestEvent, GameRequestState> {
     GameRequestEvent event,
   ) async* {
     // TODO: implement mapEventToState
-    
-    if(event is FetchCharacter){
+
+    if (event is FetchCharacter) {
       yield GameRequestLoading(loadingMessage: 'Now Loading');
       var dio = Dio();
 
-      var response = await dio.get('https://ap.api.riotgames.com/val/content/v1/contents',
-      options: Options(headers: {
-           "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0",
-    "Accept-Language": "en-US,en;q=0.5",
-    "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-    "Origin": "https://developer.riotgames.com",
-    "X-Riot-Token": "RGAPI-4ac21b1b-eded-4713-8aff-3ca3657a67ec"
-        }));
-    
+      var response =
+          await dio.get('https://ap.api.riotgames.com/val/content/v1/contents',
+              options: Options(headers: {
+                "User-Agent":
+                    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:83.0) Gecko/20100101 Firefox/83.0",
+                "Accept-Language": "en-US,en;q=0.5",
+                "Accept-Charset":
+                    "application/x-www-form-urlencoded; charset=UTF-8",
+                "Origin": "https://developer.riotgames.com",
+                "X-Riot-Token": "RGAPI-4ac21b1b-eded-4713-8aff-3ca3657a67ec"
+              }));
+
       var data = response.data;
 
       yield GameRequestFetch(data["characters"]);
       print(data["characters"]);
-    
     }
   }
 }
